fc4ed8baa901861a407da5c547735165743abd6c
Joakim Andersson <joakim.aandersson@live.se>
2021-08-30 10:20:27 +0200

    removed a image

--- components/about/Picture.js
+++ components/about/Picture.js
@@ -4,7 +4,6 @@ import classes from "./picture.module.css";
 function Picture(props) {
   return (
     <div className={classes.picture}>
-      <img src="./images/dot-pattern.svg" alt="dot pattern" />
       <img src="./images/open.jpg" alt="Salong Beautiful" />
     </div>
   );
